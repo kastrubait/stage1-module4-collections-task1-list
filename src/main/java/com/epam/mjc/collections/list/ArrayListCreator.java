db118212da638fe9b66da418df4ecca1d75746e3
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-11-28 14:30:58 +0300

    feat: complete ArrayListCreator module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+
+        int sizeList = sourceList.size();
+
+        ArrayList<String>  arrayList = new ArrayList<>();
+
+        for (int i = 1; i <= sizeList; ++i) {
+            if (i % 3 == 0) {
+                arrayList.add(sourceList.get(i));
+                arrayList.add(sourceList.get(i));
+            }
+        }
+        return arrayList;
     }
 }

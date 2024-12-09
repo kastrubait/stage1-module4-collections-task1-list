7a90200d1342983d036819c0769fc436af9cc32f
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-09 12:28:51 +0300

    feat: complete LinkedListCreator module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/LinkedListCreator.java
@@ -1,9 +1,21 @@
 package com.epam.mjc.collections.list;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
 public class LinkedListCreator {
     public LinkedList<Integer> createLinkedList(List<Integer> sourceList) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        for (Integer num : sourceList) {
+            if (num % 2 != 0) {
+                linkedList.addFirst(num);
+            } else {
+                linkedList.addLast(num);
+            }
+        }
+
+        return linkedList;
     }
 }

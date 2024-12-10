0169eed36f57417622c61fa6b76848e97b8a115e
Tatsiana Kastrubai <tatsiana.kastrubai@gmail.com>
2024-12-10 09:37:50 +0300

    feat: complete ListSorter module4 stage1 v.0

--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,23 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int num1 = Integer.parseInt(a);
+        int num2 = Integer.parseInt(b);
+
+        int f1 = 5 * num1 * num1 + 3;
+        int f2 = 5 * num2 * num2 + 3;
+
+        if (f1 != f2) {
+            return Integer.compare(f1, f2);
+        } else {
+            return Integer.compare(num1, num2);
+        }
     }
 }
